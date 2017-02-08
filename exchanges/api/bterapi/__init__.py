288f7de8025eecddab00b8d92632a58d8fba76c2
Gunwoo Kim <gunwooterry@gmail.com>
2017-02-09 04:31:23 +0900

    Remove old APIs / Add Bitfinex API

--- exchanges/api/bterapi/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2013 Alan McIntyre
-
-from .public import getDepth, getTradeHistory, get_market_cap, get_min_volumes
-from .trade import TradeAPI
-from .keyhandler import KeyHandler
-from .common import all_currencies, all_pairs, max_digits, formatCurrency, fees, formatCurrencyDigits, \
-    truncateAmount, truncateAmountDigits, BTERConnection
