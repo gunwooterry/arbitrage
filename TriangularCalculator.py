1c6c7ce1455bd468e2e6999652aa80e99cc54189
a3626a <a3626a@naver.com>
2017-02-08 20:04:44 +0900

    Bug Fix
    Bad Indentation

--- TriangularCalculator.py
+++ TriangularCalculator.py
@@ -212,6 +212,6 @@ class TriangularCalculator(object):
             if spread > 1:
                 self.log.info('check_profit_oneway({}, {}, {}) : {}'.format(self.target, B, C, spread))
             if spread > 1.001:
-               self.check_roundtrip(self.target, B, C)
+                self.check_roundtrip(self.target, B, C)
 
         #TODO: Return orders to place
