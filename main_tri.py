493b402514525128ff0eb7b60a20c341058a966b
a3626a <a3626a@naver.com>
2017-02-08 19:29:10 +0900

    Restructuring

--- main_tri.py
+++ main_tri.py
@@ -1,15 +1,7 @@
-from threading import Thread
-
 import config_tri as config
 from TriangularBot import TriangularBot
 from utils_broker import create_broker
 
-
-def thread_bot(conf, broker, targets):
-    bot = TriangularBot(conf, broker, targets)
-    bot.start(config.TICK_PERIOD)
-
 for xchg in config.EXCHANGES:
     broker = create_broker('PAPER', xchg)
-    t = Thread(target=thread_bot, args=(config, broker, config.TARGETS[xchg]))
-    t.start()
+    TriangularBot(broker, config.TARGETS[xchg], config.TICK_PERIOD).start()
